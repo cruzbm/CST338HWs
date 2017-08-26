4fe85159d26787ef94a5b45ff044a0be6b1757b5
Brandon Cruz <brandoncruz@Brandons-MacBook-Pro.local>
2017-08-26 12:04:24 -0700

    Changes made 1

--- Homework1_Cruz/src/Histogram.java
+++ Homework1_Cruz/src/Histogram.java
@@ -8,6 +8,12 @@
 * Date: 08/25/2017
 */
 
+//File imports----
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+//----------------
+
 public class Histogram {
 
 	//Faster println call
@@ -15,8 +21,36 @@ public class Histogram {
 		System.out.println(line);
 	}
 	
+	//Processor Methods-----
+	public static int counter(String str, String seeker) {
+		return 0;
+	}
+	
+	public static void printFrame() {
+		
+	}
+	
 	public static void main(String[] args) {
-		System.out.print("Input filename: ");	
+		//Initialize variables
+		Scanner keyboard;
+		String keyIn;
+		Scanner fileIn = null;
+		
+		System.out.print("Input filename: ");
+		keyboard = new Scanner(System.in);
+		keyIn = new String(keyboard.nextLine());
+		
+		//Attempt to open file
+		try {
+			//Note: file must exist outside of src
+			fileIn = new Scanner(new FileInputStream(keyIn));
+		}
+		catch (FileNotFoundException e) {
+			println("File not found.");
+			System.exit(0);
+		}
+		
+		//println("Success");
 		
 	}
 
