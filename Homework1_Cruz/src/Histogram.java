9e898e4beb358daee6704ffe6adf373156a4de8c
cruzbm <brcruz@csumb.edu>
2017-08-26 11:13:03 -0700

    Homework1_Cruz

---
+++ Homework1_Cruz/src/Histogram.java
@@ -0,0 +1,23 @@
+
+/*
+* Title: Histogram.java
+* Abstract: Displays the number of distinct characters in
+* a user defined file.
+* Author: Brandon Cruz
+* ID: 6809
+* Date: 08/25/2017
+*/
+
+public class Histogram {
+
+	//Faster println call
+	public static void println(String line) {
+		System.out.println(line);
+	}
+	
+	public static void main(String[] args) {
+		System.out.print("Input filename: ");	
+		
+	}
+
+}
