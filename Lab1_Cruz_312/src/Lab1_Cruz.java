2eb5af604e6b7380502f107cb17ec12e16a0a716
Brandon Cruz <brandoncruz@Brandons-MacBook-Pro.local>
2017-09-07 23:02:24 -0700

    nearly completed lab, need to adjust and confirm wraparound

--- Lab1_Cruz_312/src/Lab1_Cruz.java
+++ Lab1_Cruz_312/src/Lab1_Cruz.java
@@ -12,22 +12,75 @@ import java.util.InputMismatchException;
 
 public class Lab1_Cruz {
 	
+	public static String encrypt(String pt, int k) {
+		String ct = ""; //product
+		pt = pt.toLowerCase();
+		char []plain = pt.toCharArray();
+		for (int i = 0; i < plain.length; i++) {
+			int newLet = (int)plain[i];
+			if ((newLet + k) > 122) {
+				int hold = 122 - newLet + 97 ;
+				newLet = hold; //wraparound INCOMPLETE does not wrap beyond 'a'
+			}
+			else {
+				newLet += k;
+			}
+			plain[i] = (char)newLet;
+		}
+		for (int i = 0; i < plain.length; i++) {
+			ct += plain[i];
+		}
+		System.out.println("Output Ciphertext: " + ct);
+		return ct;
+	} // close encrypt
+	
+	public static String decrypt(String ct, int k) {
+		String pt = "";
+		ct = ct.toLowerCase();
+		char []cipher = ct.toCharArray();
+		for (int i = 0; i < cipher.length; i++) {
+			int newLet = (int)cipher[i];
+			if ((newLet - k) < 97) {
+				newLet = (123 - (newLet - 96)); //WRAP AROUND NOT WORKING PROPERLY
+			}
+			else {
+				newLet -= k; // 
+			}
+			cipher[i] = (char)newLet;
+		}
+		for (int i = 0; i < cipher.length; i++) {
+			pt += cipher[i];
+		}
+		System.out.println("Output Plaintext: " + pt);
+		return pt;
+	} // close encrypt
+	
 	
 	public static void main(String []args) {
-		System.out.print("Enter a string: ");
-		Scanner keyboard = new Scanner(System.in);
-		String inputString = keyboard.nextLine();
 		
-		System.out.println("");
-			
-		System.out.print("Enter a key: ");
-		keyboard = new Scanner(System.in);
-		try {
+		System.out.println("Choose an option: ");
+		System.out.print("1. Encrypt, 2. Decrypt --> ");
+		Scanner keyboard = new Scanner(System.in);
+		int opt = keyboard.nextInt();
+		
+		
+		if (opt == 1) {
+			System.out.print("Input plaintext: ");
+			keyboard = new Scanner(System.in);
+			String pt = keyboard.nextLine();
+			System.out.print("Enter a key: ");
+			keyboard = new Scanner(System.in);
 			int key = keyboard.nextInt();
+			encrypt(pt, key);
 		}
-		catch (InputMismatchException i) {
-			System.out.println("Invalid input type.");
-			return;
+		if (opt == 2) {
+			System.out.println("Input ciphertext: ");
+			keyboard = new Scanner(System.in);
+			String ct = keyboard.nextLine();
+			System.out.print("Enter the key: ");
+			keyboard = new Scanner(System.in);
+			int key = keyboard.nextInt();
+			decrypt(ct, key);
 		}
 	} // close main
 } // close class
