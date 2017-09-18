90cc1a190a949a1eb79cd927958d3f5d683ed7f3
Brandon Cruz <brandoncruz@Brandons-MacBook-Pro.local>
2017-09-17 22:19:19 -0700

    Recreated homework 4

--- Homework4_Cruz/src/Hangman.java
+++ Homework4_Cruz/src/Hangman.java
@@ -1,26 +1,6 @@
-/*
- * Title: Hangman.java
- * Abstract: This program will allow for the playing
- * of hangman based off words read in from a text file.
- * Author: Brandon Cruz
- * ID: 8309
- * Date: 9/17/17
- */
 
 
-import java.util.Scanner;
-
-import java.util.ArrayList;
 
 public class Hangman {
 
-
-
-    public static void main(String[] args){
-        System.out.println("----------Hangman----------");
-        System.out.print("Input filename: ");
-        Scanner keyboard = new Scanner(System.in);
-
-
-    }
 }
