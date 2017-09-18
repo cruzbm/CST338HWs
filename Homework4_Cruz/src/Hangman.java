7aac2aa49d35b42244737be6402e18e2c475c3e6
cruzbm <brcruz@csumb.edu>
2017-09-17 21:32:23 -0700

    Homework 4 created

---
+++ Homework4_Cruz/src/Hangman.java
@@ -0,0 +1,13 @@
+/*
+ * Title: Hangman.java
+ * Abstract: This program will allow for the playing
+ * of hangman on 
+ * Author: Brandon Cruz
+ * ID: 8309
+ * Date:
+ */
+
+
+public class Hangman {
+
+}
