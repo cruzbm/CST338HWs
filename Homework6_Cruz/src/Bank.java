c371c0fe01c389854edea2c850feb465609445df
cruzbm <brcruz@csumb.edu>
2017-10-09 15:14:08 -0700

    Adjustments made

--- Homework6_Cruz/src/Bank.java
+++ Homework6_Cruz/src/Bank.java
@@ -4,15 +4,28 @@ import java.util.HashMap;
 public class Bank {
 
     private String bankName;
-
+    private HashMap<String, Customer> customers;
+    private HashMap<String, Account> accounts;
     public Bank(String name){}
 
-    public void readData(String fileIn){}
+    public void readData(String fileIn){
+    	
+    }
 
     public void bankInfo(){}
 
-    public void accountInfo(){}
-
-
+    public void accountInfo(int num){}
+    
+    public boolean deposit(int num, double amount) {
+    	return false;
+    }
+    
+    public boolean withdraw(int num, double amount) {
+    	return false;
+    }
+    
+    public boolean closeAccount(int num) {
+    	return false;
+    }
 
 }
